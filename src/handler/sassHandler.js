d595a243c03fd532932b26fd710e1f6b7d8d8577
kariymam <59487728+kariymam@users.noreply.github.com>
2024-12-04 15:41:11 -0500

    add goals to sassHandlers.js

--- src/handler/sassHandler.js
+++ src/handler/sassHandler.js
@@ -3,31 +3,38 @@ const path = require("path");
 const { SassString } = sass;
 
 module.exports = {
-		outputFileExtension: "css",
+  outputFileExtension: "css",
 
-		compile: async function (inputContent, inputPath) {
-			let parsed = path.parse(inputPath);
-			let result = sass.compileString(inputContent, {
-				loadPaths: [
-					parsed.dir || ".",
-					this.config.dir.includes
-				],
-				functions: {
-					"mdi-svg($name, $fallback)": (args) => {
-						const nameValue = args.getValue();
-						const [named, ...fallback] = nameValue.split(/\s+/);
-						return new SassString(`url(${named}${fallback})`);
-					},
-				},
-			});
+  compile: async function (inputContent, inputPath) {
+    let parsed = path.parse(inputPath);
+    let result = sass.compileString(inputContent, {
+      loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      functions: {
+        /*
+						Goals:
+						1. get the $name argument from the function
+						2. search the src/assets/svgs
+						3. match the first word in the svg file name to the $name in the sass function args
+						4. return the path for the svg file 
+						5. return as attribute (not as a string)
+							return new SassString(`url(${args})`, {quotes: false});
+						5. save the path as an argument in "fallback"
+					*/
+        "mdi-svg($name)": (args) => {
+          return new SassString(`url(${args})`, {quotes: false});
+        },
+      },
+    });
 
-			let dependencies = result.loadedUrls.filter(dep => dep.protocol === "file:").map(entry => {
-				return path.relative(".", entry.pathname);
-			});
-			this.addDependencies(inputPath, dependencies);
+    let dependencies = result.loadedUrls
+      .filter((dep) => dep.protocol === "file:")
+      .map((entry) => {
+        return path.relative(".", entry.pathname);
+      });
+    this.addDependencies(inputPath, dependencies);
 
-			return async (data) => {
-				return result.css
-			};
-		},
+    return async (data) => {
+      return result.css;
+    };
+  },
 };
